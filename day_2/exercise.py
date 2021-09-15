1d85bc114ff92aade90c39bc1e18f2995252e95b
darrenwshort <w1765868@apps.losrios.edu>
2021-09-14 17:37:11 -0700

    working on coding poke api exercise

--- day_2/exercise.py
+++ day_2/exercise.py
@@ -4,6 +4,43 @@
 
 #PSEUDO-CODE:
 
+# IMPORTS
+import requests
+import json
+from openpyxl import Workbook
+from openpyxl.styles import Font
+from pprint import pprint
+
 #GET NAME AND ABILITY FROM API
+### api for name & ability ----->  https://pokeapi.co/api/v2/ability
+url_ability = "https://pokeapi.co/api/v2/ability"
+
+# execute call to poke api.
+response = requests.get(url_ability)
+
+# convert and save response into dict
+json_data = json.loads(response.text)
+
+# grab only 'results' item from json_data; 'results' is a list of dicts (results['name'] and results['url'])
+results = json_data['results']
+
+### TESTING - pretty print data set just to see what we're working with ###
+pprint(results)
+
+
+# create workbook/spreadsheet file.
+wb = Workbook()
+
+# create output file
+outfile = "pokeapi.xlsx"
+
+
+
+
+
+
+
+
+
 #PUT INFO IN DICTIONARY
 #ADD THE DICTIONARY TO A NEW EXCEL WORKBOOK
